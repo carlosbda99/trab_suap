4170084741c2c8a72230f3914679d0d8e6c0ddf9
Carlos Henrique <carlosbda99@hotmail.com>
2017-07-25 15:43:53 -0300

    Implementação inicial

---
+++ main_suap.py
@@ -0,0 +1,41 @@
+import sys
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from PyQt4 import uic
+import json
+import requests
+from getpass import getpass
+from os import system
+from time import sleep
+op = 0
+login = ''
+senha = ''
+ano = ''
+per = ''
+
+class boletim(QDialog):
+	def __init__ (self,parent=None):
+		super(boletim,self).__init__(parent)
+		uic.loadUi('tela3.ui',self)
+		global ano, per
+		self.meuboletim.addItem('Período: ' + ano + '.' + per)
+		self.back.clicked.connect(self.voltar)
+		self.end.clicked.connect(self.close)
+
+		resp = requests.get('https://suap.ifrn.edu.br/api/v2/minhas-informacoes/boletim/%s/%s/'%(ano,per), auth=(login,senha))
+		if resp.status_code == 200:
+			notas = resp.json()
+			for x in notas:
+				self.meuboletim.addItem(x['disciplina'])
+				self.meuboletim.addItem('\t\tNota 1: ' + str(x['nota_etapa_1']['nota']) +'\t\t\tNota 2: ' + str(x['nota_etapa_2']['nota']))
+				self.meuboletim.addItem('\t\tNota 3: ' + str(x['nota_etapa_3']['nota']) +'\t\t\tNota 4: ' + str(x['nota_etapa_4']['nota']))
+				self.meuboletim.addItem('\t\tMédia Final: '+ str(x['media_final_disciplina']))
+	
+	def close(self):
+		sys.exit()
+
+	def voltar(self):
+		t=SUAP(self)
+		t.activateWindow()
+		self.hide()
+		t.exec_()
